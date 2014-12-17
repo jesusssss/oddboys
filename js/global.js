3622a294a1298216bb43a1725bcc80db959593ff
jesusssss <reped126@hotmail.com>
2014-12-17 22:54:56 +0100

    HTML markup

---
+++ js/global.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.noise').noisy({
+        'intensity' : 1,
+        'size' : 200,
+        'opacity' : 0.1,
+        'fallback' : '',
+        'monochrome' : false
+    });
+
+    $(".cat").hover(function() {
+       $(".cat").not($(this)).addClass("greyScale");
+    }, function() {
+        $(".cat").removeClass("greyScale");
+    });
+});
