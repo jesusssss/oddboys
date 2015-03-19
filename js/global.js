9fc5fe47701a4b72dfcd97834afa7688eb7478ba
Rene <rbp@tigermedia.dk>
2015-03-19 22:48:22 +0100

    Work work

--- js/global.js
+++ js/global.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    $('.noise').noisy({
-        'intensity' : 1,
-        'size' : 200,
-        'opacity' : 0.1,
-        'fallback' : '',
-        'monochrome' : false
-    });
+    //$('.noise').noisy({
+    //    'intensity' : 1,
+    //    'size' : 200,
+    //    'opacity' : 0.1,
+    //    'fallback' : '',
+    //    'monochrome' : false
+    //});
 
     $(".cat").hover(function() {
        $(".cat").not($(this)).addClass("greyScale");
